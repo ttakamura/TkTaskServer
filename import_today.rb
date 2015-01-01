25a5ac91e904e376d076e34793b8f8e9cee66fdb
Tatsuya Takamura <takamura@tonchidot.com>
2015-01-01 18:43:23 +0900

    update

--- import_today.rb
+++ import_today.rb
@@ -5,14 +5,15 @@ require 'org-ruby'
 def parse_task line, index
   raise "No ID!! Please set ID in properties by org-mobile-push" unless line.property_drawer['ID']
   {
-    :name      => line.headline_text,
-    :section   => 1,
+    :name      => Digest::SHA1.hexdigest( line.headline_text ),
+    :section   => 2,
     :elapsed   => 0,
+    :rec_start => '',
     :done      => false,
     :date      => Time.now.to_s,
     :estimate  => 60,
     :row       => index,
-    :id        => @namespace + line.property_drawer['ID']
+    :my_id     => @namespace + line.property_drawer['ID']
   }
 end
 
@@ -38,34 +39,39 @@ end
 
 def local_db
   @local_db ||= begin
-                  remote_db, local_db = DB.open :default
+                  remote_db, local_db = DB.open :jinseitask
                   local_db.sync!
                   local_db
                 end
 end
 
 def save_task task
-  task = if current_task = Task.find(task[:id])
+  task = if current_task = Task.find(task[:my_id])
            current_task.record.data = task
            current_task
          else
            Task.new task
          end
   task.save!
+  local_db.sync!
   task
 end
 
 # ------------------------ main -------------------------
 
+Task.db = local_db
+
 file_name = ARGV.shift
 raise "Please input the file_name" unless file_name
 
-@namespace = "20141231__"
+@namespace = "20141231_2_"
 
 tasks = parse_tasks(file_name)
 
 tasks.each do |task|
   save_task(task)
 end
+
+local_db.sync!
 
 # binding.pry
