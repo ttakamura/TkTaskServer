41ce3ff87fef3affe73beb63c56b0e548c0df8ff
Tatsuya Takamura <takamura@tonchidot.com>
2015-01-06 05:25:58 +0900

    reset all

--- import_today_from_emacs.sh
+++ import_today_from_emacs.sh
@@ -2,4 +2,4 @@
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 cd /Users/tatsuya/src/github.com/ttakamura/TkTaskServer
-source /Users/tatsuya/src/github.com/ttakamura/TkTaskServer/.env && bundle exec ruby import_today.rb -f /tmp/my_org_mobile_push.org
+source /Users/tatsuya/src/github.com/ttakamura/TkTaskServer/.env && bundle exec ruby import_today.rb -f /tmp/my_org_mobile_push.org -r true
