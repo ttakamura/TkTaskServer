59b6aff8ca40ff99f321cabe9115ac5f37fe2186
Tatsuya Takamura <takamura@tonchidot.com>
2015-01-20 06:58:26 +0900

    [from now] Tue Jan 20 06:58:26 JST 2015

--- app/org_schedule.rb
+++ app/org_schedule.rb
@@ -11,17 +11,23 @@ class OrgSchedule
   private
   def parse_next_start_time text
     time = parse_time text
-    Time.parse time[:date] + " " + time[:start_time] if time[:start_time]
+    if time[:start_time]
+      Time.parse time[:date] + " " + time[:start_time]
+    else
+      Time.parse time[:date] + " 23:00"
+    end
   end
 
   def parse_next_end_time text
     time = parse_time text
-    Time.parse time[:date] + " " + time[:end_time]   if time[:end_time]
+    if time[:end_time]
+      Time.parse time[:date] + " " + time[:end_time]
+    end
   end
 
   def parse_time text
     # <2015-01-17 Sat 14:00-19:40>
-    if m = text.match(/^<(\d{4}-\d{2}-\d{2}) .+? ?(\d{2}:\d{2})?(-(\d{2}:\d{2}))? ?(.+)?>$/)
+    if m = text.match(/^<(\d{4}-\d{2}-\d{2}) [^\s]+ ?(\d{2}:\d{2})?(-(\d{2}:\d{2}))? ?(.+)?>$/)
       all, date, start_time, x, end_time, repeat_rule = m.to_a
       {date: date, start_time: start_time, end_time: end_time, repeat: repeat_rule}
     end
