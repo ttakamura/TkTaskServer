1c07576aef2e3f76d24e9ef0862e8358f43e28f0
Tatsuya Takamura <takamura@tonchidot.com>
2015-01-24 06:16:05 +0900

    Indent

--- app/org_exporter.rb
+++ app/org_exporter.rb
@@ -10,7 +10,9 @@ class OrgExporter
     end
 
     def tags_to_s
-      tags.empty? ? '' : "   :#{tags.join(':')}:"
+      text = tags.empty? ? '' : ":#{tags.join(':')}:"
+      sep  = " " * (65 - title.length)
+      sep + text
     end
 
     def schedule_to_s
@@ -18,6 +20,7 @@ class OrgExporter
     end
 
     def clock_logs_to_s
+      return "" if clock_logs.empty?
       text = []
       text << ':LOGBOOK:'
       clock_logs.map do |l|
@@ -53,42 +56,20 @@ class OrgExporter
 
   def print_headline headline
     # task = Task.find_by(id: headline.id)
-    puts headline.to_s
-    puts headline.schedule_to_s
-    puts headline.clock_logs_to_s
-    puts headline.properties_to_s
-    puts headline.body_to_s
+    print_text headline.to_s,            0
+    print_text headline.schedule_to_s,   headline.level
+    print_text headline.clock_logs_to_s, headline.level
+    print_text headline.properties_to_s, headline.level
+    print_text headline.body_to_s,       0
   end
 
-  # def print_line line, headline, task
-  #   case line.paragraph_type
-  #   when :heading1
-  #     if task
-  #       puts line.to_s.gsub(/(TODO|DONE|WAIT)/, task.done_as_text)
-  #     else
-  #       puts line.to_s
-  #     end
-  #
-  #   when :metadata
-  #     if line.to_s =~ /CLOCK: /
-  #     # TODO: Task#clocks ができたら出力して良い
-  #     elsif line.to_s =~ /SCHEDULED: /
-  #       puts line.to_s
-  #
-  #       if task && task.elapsed > 0
-  #         # CLOCK: [2015-01-04 Sun 08:49]--[2015-01-04 Sun 09:05] =>  0:16
-  #         start_at  = parse_metadata_line(line)[:scheduled_date]
-  #         end_at    = start_at             + task.elapsed
-  #         span_time = Time.new(2014, 1, 1) + task.elapsed
-  #         puts "  CLOCK: [#{ start_at.strftime(EMACS_DATE_FORMAT) }]--[#{ end_at.strftime(EMACS_DATE_FORMAT) }] => #{ span_time.strftime("%H:%M") }"
-  #       end
-  #
-  #     else
-  #       puts line.to_s
-  #     end
-  #
-  #   else
-  #     puts line.to_s
-  #   end
-  # end
+  def print_text text, level
+    text.split("\n").each do |line|
+      print_line line, level
+    end
+  end
+
+  def print_line line, level=1
+    puts "#{ '  ' * level }#{ line }"
+  end
 end
