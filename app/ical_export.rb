db889f10c1bfefda8eb991654e88704b2654cf83
Tatsuya Takamura <takamura@tonchidot.com>
2015-02-05 09:32:06 +0900

    [from now] Thu Feb  5 09:32:06 JST 2015

--- app/ical_export.rb
+++ app/ical_export.rb
@@ -3,12 +3,30 @@ class IcalExporter
     @headlines = top_org_headline.headlines
   end
 
+  def add_clock_logs headline
+    headline.clock_logs.each do |log|
+      e = Icalendar::Event.new
+      e.dtstart     = log.start_time
+      e.dtend       = log.end_time
+      e.summary     = headline.title
+      # e.description = ""
+      calendar.add_event(e)
+    end
+  end
+
+  def to_s
+    calendar.to_ical
+  end
+
+  private
   def calendar
-    cal         = Icalendar::Calendar.new
-    tz          = TZInfo::Timezone.get 'Asia/Tokyo'
-    event_start = DateTime.new 2008, 12, 29, 8, 0, 0
-    timezone    = tz.ical_timezone event_start
-    cal.add_timezone timezone
-    cal
+    @calendar ||= begin
+                    cal         = Icalendar::Calendar.new
+                    tz          = TZInfo::Timezone.get 'Asia/Tokyo'
+                    event_start = DateTime.new 2008, 12, 29, 8, 0, 0
+                    timezone    = tz.ical_timezone event_start
+                    cal.add_timezone timezone
+                    cal
+                  end
   end
 end
